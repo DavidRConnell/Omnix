b1e00b2c65f27b4b708a15e34e793b3e0b351b12
David R Connell <david32@dcon.addy.io>
2025-11-20 20:46:19 -0600

    Set up environment and describe project

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Omni exporter for Org files";
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default =
+        pkgs.mkShellNoCC { packages = (with pkgs; [ gnumake ]); };
+    };
+}
